2600cba9eae035bc4854aa4fb85276eedbd5ae6c
Santiago-scordia <santiscordia@est.frba.utn.edu.ar>
2022-04-05 17:23:05 -0300

    ItemCount

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,15 +1,18 @@
-import { Card } from 'react-bootstrap';
+import ItemCount from './ItemCount';
 
+const ItemListContainer = () => {
 
+    const onAdd = (qty) => {
+        alert("Agregaste " + qty + " items al carrito.");
+    }
 
-function Itemlistcontainer() {
     return (
-      <>
-        <Card>
-            <Card.Body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Body>
-        </Card>
-      </>
+        <>
+            
+            <ItemCount stock={10} initial={1} onAdd={onAdd} />
+        </>
     );
-  }
-  
-  export default Itemlistcontainer;
+    
+}
+
+export default ItemListContainer;
