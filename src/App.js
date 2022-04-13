171d8f70d51c2066f91ef8b2ce8b2cc8f708db23
Santiago-scordia <santiscordia@est.frba.utn.edu.ar>
2022-04-13 18:08:30 -0300

    ItemId

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import './App.css';
 import NavBar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ItemDetailContainer from './components/ItemDetailContainer';
+import ItemListContainer from './components/ItemListContainer';
 
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <NavBar />
-      <ItemDetailContainer />
+          <Routes>
+              <Route path='/' element={<ItemListContainer />} />
+              <Route path='/category/:idCategory' element={<ItemListContainer />} />
+              <Route path='/item/:idItem' element={<ItemDetailContainer />} />
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
